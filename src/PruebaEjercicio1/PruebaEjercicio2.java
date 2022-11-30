a639af6c921af6b896dea5f88dc8b2d0f60754d9
“Deakon07git config --global user.email “fjavierguillenpuente@gmail.com <fjavierguillenpuente@gmail.com>
2022-11-30 10:32:49 +0100

    Se modifica la impresión con el día de hoy 30 de noviembre

--- src/PruebaEjercicio1/PruebaEjercicio2.java
+++ src/PruebaEjercicio1/PruebaEjercicio2.java
@@ -3,7 +3,7 @@ package PruebaEjercicio1;
 public class PruebaEjercicio2 {
 
 	public static void main(String[] args) {
-		System.out.println("Esto es otra prueba");
+		System.out.println("Hoy es 30 de noviembre");
 
 	}
 
